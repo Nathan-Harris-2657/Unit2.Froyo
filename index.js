4c587b172c90b134451f7324fc44ed4efb0efb60
Nathan Harris <singdubharris@gmail.com>
2025-03-25 19:25:33 -0400

    finished

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 // Prompt User to type flavors they ordered
 const userInputFlavors = prompt(
-    "Test"
+    "please enter your froyo order with (,) inbetween each flavor"
 )
 
+
 // Take users input and truns it in to array then to Object
-const froyoObj ={...userInputFlavors.split(",")}
+const froyoArray = userInputFlavors.split(",")
+
+
+// A loop is used to iterate through the array of flavors.
    
-function froyoValue(){
-    
+
+
+
+
+// froyoArray to object 
+// [vanilla,vanilla,cherry,coffee,coffee]
+// return should be an object with flavors as keys and count of flavors as values
+/**
+ * 
+ * {
+ *  "vanilla": 2,
+ *  "cherry": 1,
+ * }
+ */
+const countFlavors = () =>{
+    const obj = {} 
+    for(let i = 0; i < froyoArray.length; i++){
+        const flavor = froyoArray[i]
+        //does my object have the flavor vanilla
+        if(obj[flavor]){
+            //obj["vanilla"] = obj["vanilla"] + 1;
+            obj[flavor]++;
+        }else {
+            //obj["vanilla"] = 1
+            obj[flavor] = 1;
+        }
+    }
+    return obj
 }
 
-console.table(froyoObj)
+
+
+// creating object with the fuction "creatObject"
+const froyoObj = countFlavors()
+
+
+
+console.table(Object.keys(froyoObj), Object.values(froyoObj) )
