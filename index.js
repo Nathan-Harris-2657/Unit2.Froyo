cd03be91615d0544503661c9278cfa99a142dda2
Nathan Harris <singdubharris@gmail.com>
2025-03-24 22:43:54 -0400

    started

---
+++ index.js
@@ -0,0 +1,13 @@
+// Prompt User to type flavors they ordered
+const userInputFlavors = prompt(
+    "Test"
+)
+
+// Take users input and truns it in to array then to Object
+const froyoObj ={...userInputFlavors.split(",")}
+   
+function froyoValue(){
+    
+}
+
+console.table(froyoObj)
